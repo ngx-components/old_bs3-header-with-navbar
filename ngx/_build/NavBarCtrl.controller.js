3daabdafd6558d47e2bed50bb55018b6ddb6384f
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-09-10 21:41:32 +0200

    Initial commit

---
+++ ngx/_build/NavBarCtrl.controller.js
@@ -0,0 +1,21 @@
+(function(){
+
+  /**
+   * Controller for navigation bar
+   *
+   * @param $scope
+   * @param $translate
+   * @constructor
+   */
+  function NavBarCtrl(){
+  }
+
+  // Inject dependencies
+  NavBarCtrl.$inject = [];
+
+  // Export
+  angular
+    .module('app')
+    .controller('NavBarCtrl', NavBarCtrl);
+
+})();
